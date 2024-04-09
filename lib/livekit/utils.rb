92e84afa3ca7bd1060ccc23bbfec10202c7826a1
amerov <maratamerov@gmail.com>
2024-04-10 01:46:55 +0500

    Don't pollute Object class

--- lib/livekit/utils.rb
+++ lib/livekit/utils.rb
@@ -16,12 +16,18 @@ unless Hash.method_defined?(:stringify_keys)
   end
 end
 
-# convert websocket urls to http
-def to_http_url(url)
-  if url.start_with?("ws")
-    # replace ws prefix to http
-    return url.sub(/^ws/, "http")
-  else
-    return url
+module LiveKit
+  module Utils
+    def to_http_url(url)
+      if url.start_with?("ws")
+        # replace ws prefix to http
+        return url.sub(/^ws/, "http")
+      else
+        return url
+      end
+    end
+
+    module_function :to_http_url
   end
 end
+# convert websocket urls to http
