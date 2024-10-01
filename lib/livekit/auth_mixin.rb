3ab234d5e88e402d60cde4a9849e5f4877b13f55
Benjamin Pracht <benjamin@livekit.io>
2024-10-01 14:52:15 -0700

    WiP

--- lib/livekit/auth_mixin.rb
+++ lib/livekit/auth_mixin.rb
@@ -3,10 +3,16 @@
 module LiveKit
   # Create authenticated headers when keys are provided
   module AuthMixin
-    def auth_header(video_grant)
+    def auth_header(
+      video_grant,
+      sip_grant: nil
+    )
       headers = {}
       t = ::LiveKit::AccessToken.new(api_key: @api_key, api_secret: @api_secret)
       t.add_grant(video_grant)
+      if sip_grant != nil
+        t.add_sip_grant(sip_grant)
+      end
       headers["Authorization"] = "Bearer #{t.to_jwt}"
       headers["User-Agent"] = "LiveKit Ruby SDK"
       headers
